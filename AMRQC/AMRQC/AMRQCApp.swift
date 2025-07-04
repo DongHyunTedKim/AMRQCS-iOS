58d0a4313975f9e9f24b492953734b954011eb34
TedKim <eastar29@naver.com>
2025-07-04 14:43:02 +0900

    init Xcode project

---
+++ AMRQC/AMRQC/AMRQCApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMRQCApp.swift
+//  AMRQC
+//
+//  Created by Ted Kim on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AMRQCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
